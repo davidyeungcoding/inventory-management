e8bd646462e78d5c2de910fcf4332b81a1a6400d
davidyeungcoding <yeungpirate@gmail.com>
2022-07-21 10:22:41 -0700

    Reworked delete item feature to display success or failure message. Refined handling for delete request.

--- src/app/components/item/item-list/item-list.component.ts
+++ src/app/components/item/item-list/item-list.component.ts
@@ -14,6 +14,7 @@ export class ItemListComponent implements OnInit, OnDestroy {
   private subscriptions = new Subscription();
   itemList: Item[] = [];
   targetItem: Item|null = null;
+  deleteMessage: String = '';
 
   constructor(
     private itemService: ItemService
@@ -39,15 +40,29 @@ export class ItemListComponent implements OnInit, OnDestroy {
     });
   };
 
+  swapClass(remove: string, add: string, target: string): void {
+    $(`${target}`).removeClass(remove);
+    $(`${target}`).addClass(add);
+  };
+
   removeItemFromList(item: Item): Item[] {
     let temp = this.itemList;
 
     for (let i = 0; i < temp.length; i++) {
-      const _item = temp[i];
-      if (_item._id === item._id) temp.splice(i, 1);
+      if (temp[i]._id === item._id) temp.splice(i, 1);
+      break;
     };
 
     return temp;
+  };
+
+  removeItemFromIngredient(): void {
+    // handle removing item from all ingredients in ingredient list
+  }
+
+  onTargetItem(item: Item, target: string): void {
+    $(`${target}`).css('display', 'none');
+    this.targetItem = item;
   };
 
   // =======================
@@ -61,25 +76,33 @@ export class ItemListComponent implements OnInit, OnDestroy {
     });
   };
 
-  onMarkForEdit(item: Item): void {
-    this.targetItem = item;
-  };
+  onDelete(): void {
+    this.itemService.deleteItem(this.targetItem!).subscribe(_res => {
+      this.deleteMessage = _res.msg;
 
-  onDelete(item: Item): void {
-    this.itemService.deleteItem(item).subscribe(_res => {
       if (_res.status === 200) {
-        this.itemService.changeItemList(this.removeItemFromList(item));
-        // display success message
-      }
-      // handle error with deletion here
+        this.itemService.changeItemList(this.removeItemFromList(this.targetItem!));
+        this.swapClass('alert-danger', 'alert-success', '#deleteResult');
+        $('#deleteMsgContainer').css('display', 'inline');
+        
+        setTimeout(() => {
+          (<any>$('#deleteItem')).modal('hide');
+        }, 1500);
+      } else {
+        this.swapClass('alert-success', 'alert-danger', '#deleteResult');
+        $('#deleteMsgContainer').css('display', 'inline');
+        // handle error with deletion here
+      };
     });
   };
 
   onBack(): void {
     console.log('back');
+    // handle go back
   };
 
   onAddItem(): void {
     console.log('new item');
+    // handle add new item
   };
 }
